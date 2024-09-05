39647d6277b0983339d6061b462fd7f8073c36bf
taohansens <taohansens@gmail.com>
2024-09-05 08:58:45 -0300

    init: first commit

---
+++ src/main/java/com/taohansen/microrabbit/MicroRabbitApplication.java
@@ -0,0 +1,13 @@
+package com.taohansen.microrabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroRabbitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroRabbitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taohansen/microrabbit/MicroRabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taohansen.microrabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroRabbitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
